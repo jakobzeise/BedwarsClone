0594ea08da851ddd957685638fee27750f4a9b4b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-07 17:48:22 +0200

    Initial commit

---
+++ src/main/java/com/jakobzeise/bedwarsclone/BedwarsClone.java
@@ -0,0 +1,17 @@
+package com.jakobzeise.bedwarsclone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedwarsClone extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
